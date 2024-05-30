d49af9e85e8942f652bb18cf8d7a0893cbd09aa8
vogiaan1904 <vogiaan1904@gmail.com>
2024-05-30 22:42:05 +0700

    change server port

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.all("*", (req, res, next) => {
 
 // Server
 
-const PORT = process.env.PORT || 5200;
+const PORT = process.env.PORT || 5201;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
